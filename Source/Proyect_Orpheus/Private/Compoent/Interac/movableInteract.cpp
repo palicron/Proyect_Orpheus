e35437612f73b182522ad362921ecca77ddfee66
palicron <elgranpalicron@gmail.com>
2021-08-01 13:25:14 -0500

    avances movement

--- Source/Proyect_Orpheus/Private/Compoent/Interac/movableInteract.cpp
+++ Source/Proyect_Orpheus/Private/Compoent/Interac/movableInteract.cpp
@@ -3,6 +3,34 @@
 
 #include "Compoent/Interac/movableInteract.h"
 
+
+
+void UmovableInteract::BeginPlay()
+{
+
+	Super::BeginPlay();
+
+	switch (Type)
+	{
+	case EMovementType::SimpleDirLinear:
+		MovePat.Add(GetOwner()->GetActorLocation());
+		MovePat.Add((DirectionVector.GetSafeNormal(0.01f) * TravelDistance) + MovePat[0]);
+		break;
+	case EMovementType::SimpleActorLinear:
+
+		break;
+	
+	}
+}
+
+void UmovableInteract::TickComponent(float DeltaTime, ELevelTick TickType,
+	FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+	
+	MoveTarger(DeltaTime);
+}
+
 void UmovableInteract::OnPress()
 {
 }
@@ -21,4 +49,19 @@ void UmovableInteract::OnDeSelectet()
 
 void UmovableInteract::ActorDectroid()
 {
+}
+
+
+
+void UmovableInteract::SimpleMoveCalculation()
+{
+	
+}
+
+void UmovableInteract::MoveTarger(float deltaTime)
+{
+	if(moving)
+	{
+		
+	}
 }
--- Source/Proyect_Orpheus/Public/Compoent/Interac/movableInteract.h
+++ Source/Proyect_Orpheus/Public/Compoent/Interac/movableInteract.h
@@ -6,9 +6,12 @@
 #include "Compoent/TIntractable.h"
 #include "movableInteract.generated.h"
 
-/**
- *
- */
+UENUM(BlueprintType)
+enum class EMovementType:uint8
+{
+	SimpleDirLinear UMETA(DisplayName = "Simple move in direction linear"),
+	SimpleActorLinear UMETA(DisplayName = "Simple move to actor linear"),
+};
 UCLASS()
 class PROYECT_ORPHEUS_API UmovableInteract : public UTIntractable
 {
@@ -16,15 +19,42 @@ class PROYECT_ORPHEUS_API UmovableInteract : public UTIntractable
 
 
 public:
-	
-		virtual void OnPress() override;
 
-		virtual void OnLongPress() override;
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Move Config")
+		AActor* Target;
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Move Config")
+		bool TwoWays = true;
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Move Config")
+		EMovementType Type = EMovementType::SimpleDirLinear;
+	UPROPERTY(VisibleDefaultsOnly, BlueprintReadOnly, Category = "Move path")
+		TArray<FVector> MovePat;
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Direction config")
+		FVector DirectionVector = FVector::ZeroVector;
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Direction config")
+		float TravelDistance = 0.0f;
+	bool moving = false;
+
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 	
-		virtual void OnSelectet() override;
+	virtual void OnPress() override;
+
+	virtual void OnLongPress() override;
+
+	virtual void OnSelectet() override;
+
+	virtual void OnDeSelectet() override;
+
+	virtual void ActorDectroid() override;
+
+protected:
+
+	virtual void BeginPlay() override;
+	//Calcular el vector de direccion
+	UFUNCTION(BlueprintCallable,Category="Direction config")
+	void SimpleMoveCalculation();
+	//Mueve el actor 
+	void MoveTarger(float deltaTime);
+
 	
-		virtual void OnDeSelectet() override;
-	
-		virtual void ActorDectroid() override;
 
 };
