37c38da199c2dab1dd281215885c37c38ef6822f
palicron <elgranpalicron@gmail.com>
2021-08-03 18:57:28 -0500

    Finish Active interactive y una puerta dummy

--- Source/Proyect_Orpheus/Private/Compoent/Interac/ActivationInterac.cpp
+++ Source/Proyect_Orpheus/Private/Compoent/Interac/ActivationInterac.cpp
@@ -3,3 +3,36 @@
 
 #include "Compoent/Interac/ActivationInterac.h"
 
+void UActivationInterac::OnPress()
+{
+}
+
+void UActivationInterac::OnLongPress()
+{
+}
+
+void UActivationInterac::OnSelectet()
+{
+	if(CanBeUse)
+	{
+		InSelectEvent.Broadcast();
+		if(OneUse)
+		{
+			CanBeUse = false;
+		}
+	}
+	else
+	{
+		OnNotActiveEvent.Broadcast();
+	}
+	
+}
+
+void UActivationInterac::OnDeSelectet()
+{
+	InDeSelectEvent.Broadcast();
+}
+
+void UActivationInterac::ActorDectroid()
+{
+}
--- Source/Proyect_Orpheus/Public/Compoent/Interac/ActivationInterac.h
+++ Source/Proyect_Orpheus/Public/Compoent/Interac/ActivationInterac.h
@@ -13,5 +13,24 @@ UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class PROYECT_ORPHEUS_API UActivationInterac : public UTIntractable
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditInstanceOnly,BlueprintReadWrite,Category="Activation Config")
+	bool OneUse = true;
+	UPROPERTY(EditInstanceOnly, BlueprintReadWrite, Category = "Activation Config")
+	bool CanBeUse = true;
+
+	UPROPERTY(BlueprintAssignable)
+		FSelectedDelegateEvent OnNotActiveEvent;
+	
+	virtual void OnPress() override;
+
+	virtual void OnLongPress() override;
+
+	virtual void OnSelectet() override;
+
+	virtual void OnDeSelectet() override;
+
+	virtual void ActorDectroid() override;
 	
 };
