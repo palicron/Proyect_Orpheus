bfffe68d9cd21141941a1a408a783c16f94fbbdf
palicron <elgranpalicron@gmail.com>
2021-08-03 17:56:38 -0500

    Create activation interact

---
+++ Source/Proyect_Orpheus/Public/Compoent/Interac/ActivationInterac.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Compoent/TIntractable.h"
+#include "ActivationInterac.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class PROYECT_ORPHEUS_API UActivationInterac : public UTIntractable
+{
+	GENERATED_BODY()
+	
+};
