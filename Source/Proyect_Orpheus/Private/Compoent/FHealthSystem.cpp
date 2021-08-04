189886cc601c5648d75427239d33290a5e90cb3c
palicron <elgranpalicron@gmail.com>
2021-08-04 12:15:50 -0500

    Skeleton for HealthSystem

--- Source/Proyect_Orpheus/Private/Compoent/FHealthSystem.cpp
+++ Source/Proyect_Orpheus/Private/Compoent/FHealthSystem.cpp
@@ -8,7 +8,9 @@ UFHealthSystem::UFHealthSystem()
 {
 	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
 	// off to improve performance if you don't need them.
-	PrimaryComponentTick.bCanEverTick = true;
+	PrimaryComponentTick.bCanEverTick = false;
+
+	CurrentHealth = MaxHealth;
 
 	// ...
 }
@@ -30,5 +32,19 @@ void UFHealthSystem::TickComponent(float DeltaTime, ELevelTick TickType, FActorC
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
 	// ...
+}
+
+int32 UFHealthSystem::TakeDmg(int32 dmg)
+{
+	return 0;
+}
+
+int32 UFHealthSystem::CureDmg(int32 cure)
+{
+	return 0;
+}
+
+void UFHealthSystem::Death()
+{
 }
 
--- Source/Proyect_Orpheus/Public/Compoent/FHealthSystem.h
+++ Source/Proyect_Orpheus/Public/Compoent/FHealthSystem.h
@@ -6,23 +6,45 @@
 #include "Components/ActorComponent.h"
 #include "FHealthSystem.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FHealthSystemEvent);
 
-UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
 class PROYECT_ORPHEUS_API UFHealthSystem : public UActorComponent
 {
 	GENERATED_BODY()
 
-public:	
+public:
 	// Sets default values for this component's properties
 	UFHealthSystem();
+	int32 MaxHealth = 100;
+
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
-public:	
+	int32 CurrentHealth;
+
+	float InvulnerabilityTime = 0.5f;
+	
+	UPROPERTY(BlueprintAssignable)
+		FHealthSystemEvent TakeDmgEvent;
+
+	UPROPERTY(BlueprintAssignable)
+		FHealthSystemEvent CureDmgEvent;
+	UPROPERTY(BlueprintAssignable)
+		FHealthSystemEvent DeathEvent;
+
+public:
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+
+	virtual int32 TakeDmg(int32 dmg);
+
+	virtual int32 CureDmg(int32 cure);
+
+	virtual void Death();
+
+
 };
